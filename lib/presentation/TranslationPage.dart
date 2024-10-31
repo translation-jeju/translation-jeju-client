ab2119a4d2bc387be304d62836676bf7b91a263c
glone0506 <glone0506@gmail.com>
2024-11-01 06:09:50 +0900

    fix:test

--- lib/presentation/TranslationPage.dart
+++ lib/presentation/TranslationPage.dart
@@ -20,7 +20,7 @@ class _TranslationPageState extends ConsumerState<TranslationPage> {
   void initState() {
     super.initState();
     WidgetsBinding.instance.addPostFrameCallback((_) {
-      ref.read(speechProvider).initialize(context); // context를 전달합니다.
+      ref.read(speechProvider).initialize(context);
     });
   }
 
@@ -28,190 +28,229 @@ class _TranslationPageState extends ConsumerState<TranslationPage> {
   Widget build(BuildContext context) {
     final speechNotifier = ref.watch(speechProvider);
     final selectedLanguage = ref.watch(languageProvider);
-    final languageName = selectedLanguage?.name ?? 'Unknown'; // 사용자가 선택한 언어이름
 
     return SafeArea(
       child: Scaffold(
         backgroundColor: Colors.white,
-        appBar: null,
         body: Padding(
           padding: const EdgeInsets.all(25.0),
           child: Column(
             mainAxisAlignment: MainAxisAlignment.center,
             children: [
-              // 인식된 텍스트를 표시하는 텍스트 상자
-              Container(
-                padding:
-                    const EdgeInsets.symmetric(horizontal: 23.0, vertical: 25),
-                width: double.infinity,
-                decoration: BoxDecoration(
-                  border: Border.all(color: AppColors.line2),
-                  borderRadius: BorderRadius.circular(20.0),
-                ),
-                child: Column(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: [
-                    Text(
-                      speechNotifier.text,
-                      style: FontStyles.largeTitle,
-                    ),
-                    const SizedBox(
-                      height: 38,
-                    ),
-                    // 음성 인식 시작/정지 버튼
-                    Row(
-                      children: [
-                        InkWell(onTap: () {}, child: SvgPicture.asset('assets/images/ic_volume_black.svg'),),
-                        const Spacer(),
-                        Padding(
-                          padding: const EdgeInsets.only(right: 10.0),
-                          child: GestureDetector(
-                            onTap: () {
-                              showModalBottomSheet(
-                                context: context,
-                                backgroundColor: Colors.white,
-                                builder: (BuildContext context) {
-                                  return Column(
-                                    mainAxisSize: MainAxisSize.min,
-                                    crossAxisAlignment:
-                                        CrossAxisAlignment.start,
-                                    children: [
-                                      Padding(
-                                        padding: EdgeInsets.only(
-                                            right: 25.0, left: 25, top: 25),
-                                        child: Text(
-                                          AppLocalizations.of(context)
-                                                  ?.all_language ??
-                                              'All language',
-                                          style: FontStyles.smallTitle,
-                                        ),
-                                      ),
-                                      const Divider(
-                                        thickness: 1,
-                                        color: AppColors.line1,
-                                      ),
-                                      InkWell(
-                                        onTap: () {},
-                                        child: SizedBox(
-                                          width: double.infinity,
-                                          child: Padding(
-                                            padding: EdgeInsets.symmetric(
-                                                vertical: 12, horizontal: 25),
-                                            child: Column(
-                                              crossAxisAlignment:
-                                                  CrossAxisAlignment.start,
-                                              children: [
-                                                Text(
-                                                  AppLocalizations.of(context)
-                                                          ?.indonesian ??
-                                                      'Indonesian',
-                                                  style: FontStyles.smallBody,
-                                                ),
-                                                Divider(
-                                                  thickness: 1,
-                                                  color: AppColors.line1,
-                                                )
-                                              ],
-                                            ),
-                                          ),
-                                        ),
-                                      ),
-                                    ],
-                                  );
-                                },
-                              );
-                            },
-                            child: Row(
-                              children: [
-                                Text(
-                                  AppLocalizations.of(context)?.indonesian ??
-                                      'Indonesian',
-                                  style: FontStyles.mediumBody,
-                                ),
-                                Icon(Icons.keyboard_arrow_down)
-                              ],
-                            ),
-                          ),
-                        ),
-                        InkWell(
-                          onTap: () {
-                            if (speechNotifier.isListening) {
-                              speechNotifier.stopListening();
-                            } else {
-                              speechNotifier.startListening(ref);
-                            }
-                          },
-                          child: Icon(speechNotifier.isListening
-                              ? Icons.mic_off
-                              : Icons.mic),
-                        ),
-                      ],
-                    ),
-                  ],
+              // Recognized text display box
+              _buildTextDisplayBox(context, speechNotifier, selectedLanguage),
+              const SizedBox(height: 20),
+              // Translated text display box
+              _buildTranslatedTextBox(context, speechNotifier),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+
+  // 텍스트 디스플레이 박스
+  Widget _buildTextDisplayBox(
+      BuildContext context, var speechNotifier, Language? selectedLanguage) {
+    String languageLabel;
+
+    switch (selectedLanguage) {
+      case Language.korean:
+        languageLabel = AppLocalizations.of(context)?.korean ?? 'Korean';
+        break;
+      case Language.english:
+        languageLabel = AppLocalizations.of(context)?.english ?? 'English';
+        break;
+      case Language.chinese:
+        languageLabel = AppLocalizations.of(context)?.chinese ?? 'Chinese';
+        break;
+      case Language.indonesian:
+        languageLabel =
+            AppLocalizations.of(context)?.indonesian ?? 'Indonesian';
+        break;
+      default:
+        languageLabel = 'Unknown';
+    }
+
+    return Container(
+      padding: const EdgeInsets.symmetric(horizontal: 23.0, vertical: 25),
+      width: double.infinity,
+      decoration: BoxDecoration(
+        border: Border.all(color: AppColors.line2),
+        borderRadius: BorderRadius.circular(20.0),
+      ),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          Text(speechNotifier.text, style: FontStyles.largeTitle),
+          const SizedBox(height: 38),
+          Row(
+            children: [
+              InkWell(
+                onTap: () {
+                  speechNotifier.speakText();
+                },
+                child: SvgPicture.asset('assets/images/ic_volume_black.svg'),
+              ),
+              const Spacer(),
+              Padding(
+                padding: const EdgeInsets.only(right: 10.0),
+                child: GestureDetector(
+                  onTap: () => _showLanguageSelectionModal(context, ref),
+                  child: Row(
+                    children: [
+                      Text(
+                        languageLabel,
+                        style: FontStyles.mediumBody,
+                      ),
+                      const Icon(Icons.keyboard_arrow_down),
+                    ],
+                  ),
                 ),
               ),
-              const SizedBox(height: 20),
-              //번역된 상태를 표시하는 텍스트 상자
-              Container(
-                padding:
-                    const EdgeInsets.symmetric(horizontal: 23.0, vertical: 25),
-                width: double.infinity,
-                decoration: BoxDecoration(
-                    borderRadius: BorderRadius.circular(20.0),
-                    color: AppColors.mainColor),
-                child: Column(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: [
-                    Text(
-                      '혼자옵서예',
-                      style: FontStyles.largeTitle.copyWith(
-                          color: Colors.white, fontFamily: 'PretendardMedium'),
-                    ),
-                    const SizedBox(
-                      height: 40,
-                    ),
-                    // 음성 인식 시작/정지 버튼
-                    Row(
-                      children: [
-                        InkWell(
-                          onTap: () async {
-                            await postTranslation(speechNotifier.text,
-                                selectedLanguage.toString());
-                          },
-                          child: SvgPicture.asset('assets/images/ic_volume_black.svg', color: Colors.white,),
-                        ),
-                        const Spacer(),
-                        Padding(
-                          padding: const EdgeInsets.only(right: 10.0),
-                          child: Text(
-                            AppLocalizations.of(context)?.jeju_language ??
-                                'Jeju language',
-                            style: FontStyles.mediumBody.copyWith(color: Colors.white),
-                          ),
-                        ),
-                        InkWell(
-                          onTap: () {
-                            if (speechNotifier.isListening) {
-                              speechNotifier.stopListening();
-                            } else {
-                              speechNotifier.startListening(ref);
-                            }
-                          },
-                          child: Icon(
-                            speechNotifier.isListening
-                                ? Icons.mic_off
-                                : Icons.mic,
-                            color: Colors.white,
-                          ),
-                        ),
-                      ],
-                    ),
-                  ],
+              InkWell(
+                onTap: () async {
+                  if (!speechNotifier.isListening) {
+                    print(2);
+                    await speechNotifier.startListening(ref);
+                  } else {
+                    print(1);
+                    await speechNotifier.stopListening();
+                  }
+                },
+                child: Icon(
+                  speechNotifier.isListening ? Icons.mic : Icons.mic_off,
                 ),
               ),
             ],
           ),
-        ),
+        ],
+      ),
+    );
+  }
+
+  // 번역된 텍스트 박스
+  Widget _buildTranslatedTextBox(BuildContext context, var speechNotifier) {
+    return Container(
+      padding: const EdgeInsets.symmetric(horizontal: 23.0, vertical: 25),
+      width: double.infinity,
+      decoration: BoxDecoration(
+        borderRadius: BorderRadius.circular(20.0),
+        color: AppColors.mainColor,
+      ),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          Text(
+            speechNotifier.translatedText.isNotEmpty
+                ? speechNotifier.translatedText
+                : '번역된 텍스트가 여기에 표시됩니다.', // 기본값 설정
+            style: FontStyles.largeTitle
+                .copyWith(color: Colors.white, fontFamily: 'PretendardMedium'),
+          ),
+          const SizedBox(height: 40),
+          Row(
+            children: [
+              InkWell(
+                onTap: () async {
+                  print(speechNotifier.translatedText);
+                  speechNotifier.speakTranslatedText();
+                },
+                child: SvgPicture.asset(
+                  'assets/images/ic_volume_black.svg',
+                  color: Colors.white,
+                ),
+              ),
+              const Spacer(),
+              Padding(
+                padding: const EdgeInsets.only(right: 10.0),
+                child: Text(
+                  AppLocalizations.of(context)?.jeju_language ??
+                      'Jeju language',
+                  style: FontStyles.mediumBody.copyWith(color: Colors.white),
+                ),
+              ),
+              InkWell(
+                onTap: () {
+                  if (speechNotifier.isListening) {
+                    speechNotifier.stopListening();
+                  } else {
+                    speechNotifier.startListening(ref);
+                  }
+                },
+                child: Icon(
+                  speechNotifier.isListening ? Icons.mic_off : Icons.mic,
+                  color: Colors.white,
+                ),
+              ),
+            ],
+          ),
+        ],
+      ),
+    );
+  }
+
+  void _showLanguageSelectionModal(BuildContext context, WidgetRef ref) {
+    showModalBottomSheet(
+      context: context,
+      backgroundColor: Colors.white,
+      shape: const RoundedRectangleBorder(
+        borderRadius: BorderRadius.vertical(top: Radius.circular(20)),
+      ),
+      builder: (BuildContext context) {
+        return Padding(
+          padding: const EdgeInsets.symmetric(vertical: 16.0),
+          child: Column(
+            mainAxisSize: MainAxisSize.min,
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Padding(
+                padding:
+                    const EdgeInsets.only(right: 25.0, left: 25.0, top: 25.0),
+                child: Text(
+                  AppLocalizations.of(context)?.all_language ?? 'All languages',
+                  style: FontStyles.smallTitle,
+                ),
+              ),
+              const Divider(thickness: 1, color: AppColors.line1),
+              _buildLanguageOption(context, ref, Language.korean, 'Korean'),
+              const Divider(
+                  thickness: 1,
+                  color: AppColors.line1,
+                  indent: 25,
+                  endIndent: 25),
+              _buildLanguageOption(context, ref, Language.english, 'English'),
+              const Divider(
+                  thickness: 1,
+                  color: AppColors.line1,
+                  indent: 25,
+                  endIndent: 25),
+              _buildLanguageOption(context, ref, Language.chinese, 'Chinese'),
+              const Divider(
+                  thickness: 1,
+                  color: AppColors.line1,
+                  indent: 25,
+                  endIndent: 25),
+              _buildLanguageOption(
+                  context, ref, Language.indonesian, 'Indonesian'),
+            ],
+          ),
+        );
+      },
+    );
+  }
+
+  // 언어 선택 옵션 위젯
+  Widget _buildLanguageOption(
+      BuildContext context, WidgetRef ref, Language language, String label) {
+    return InkWell(
+      onTap: () {
+        ref.read(languageProvider.notifier).state = language;
+        Navigator.pop(context);
+      },
+      child: Padding(
+        padding: const EdgeInsets.symmetric(vertical: 16, horizontal: 25),
+        child: Text(label, style: FontStyles.smallBody),
       ),
     );
   }
--- lib/riverpod/speech_provider.dart
+++ lib/riverpod/speech_provider.dart
@@ -2,27 +2,65 @@ import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:speech_to_text/speech_recognition_result.dart';
 import 'package:speech_to_text/speech_to_text.dart' as stt;
+import 'package:flutter_tts/flutter_tts.dart';
+
 import 'package:flutter_gen/gen_l10n/app_localizations.dart';
+import 'package:translation/data/repository/TranslationRepository.dart';
 import 'package:translation/riverpod/language_provider.dart';
 
-// 음성 인식 상태를 관리하는 Provider
 class SpeechNotifier extends ChangeNotifier {
   final stt.SpeechToText _speech = stt.SpeechToText();
+  final FlutterTts _tts = FlutterTts(); // TTS 인스턴스 생성
   bool _isListening = false;
-  String _text = 'Press the button and speak'; // 기본 값
+  String _text = 'Press the button and speak'; // 기본값
+  String _translatedText = ''; // 번역된 텍스트 저장
 
   bool get isListening => _isListening;
   String get text => _text;
+  String get translatedText => _translatedText;
+
+  // 번역 메서드
+  Future<void> translateText(String originalText) async {
+    _translatedText = await postTranslation(originalText);
+    notifyListeners(); // UI 업데이트
+  }
+
+  // TTS 초기화 및 설정
+  Future<void> initializeTTS() async {
+    await _tts.setLanguage("ko-KR"); // 기본 언어 설정
+    await _tts.setSpeechRate(0.5); // 발음 속도 설정
+    await _tts.setVolume(1.0); // 볼륨 설정
+    await _tts.setPitch(1.0); // 피치 설정
+  }
+
+  // 번역된 텍스트 음성 출력 메서드
+  Future<void> speakTranslatedText() async {
+    if (_translatedText.isNotEmpty) {
+      await _tts.speak(_translatedText); // TTS 발음 호출
+    } else {
+      print("번역된 텍스트가 없습니다."); // 번역된 텍스트가 없는 경우 로그 출력
+    }
+  }
+
+  // 번역된 텍스트 음성 출력 메서드
+  Future<void> speakText() async {
+    if (_text.isNotEmpty) {
+      await _tts.speak(_text); // TTS 발음 호출
+    } else {
+      print("말씀해주세요"); // 번역된 텍스트가 없는 경우 로그 출력
+    }
+  }
 
   // 음성 인식 초기화
   Future<void> initialize(BuildContext context) async {
-    _isListening = await _speech.initialize(); // 초기화 상태를 설정
+    _isListening = await _speech.initialize();
+    await initializeTTS(); // TTS 초기화 추가
     _text = AppLocalizations.of(context)?.press_the_button_and_speak ??
         'Press the button and speak';
     notifyListeners();
   }
 
-  // 선택된 언어에 맞는 localeId를 반환
+  // 선택된 언어의 localeId를 반환
   String _getLocaleIdFromLanguage(Language? language) {
     switch (language) {
       case Language.korean:
@@ -34,35 +72,35 @@ class SpeechNotifier extends ChangeNotifier {
       case Language.indonesian:
         return 'id_ID';
       default:
-        return 'en_US'; // 기본값을 영어로 설정
+        return 'en_US'; // 기본값 설정
     }
   }
 
   // 음성 인식 시작
-  void startListening(WidgetRef ref) async {
-    // 선택된 언어가 null일 경우 기본값을 설정합니다.
-    final selectedLanguage = ref.watch(languageProvider) ?? Language.english;
+  Future<void> startListening(WidgetRef ref) async {
+    final selectedLanguage = ref.read(languageProvider) ?? Language.english;
     final localeId = _getLocaleIdFromLanguage(selectedLanguage);
 
     if (!_isListening) {
       _isListening = true;
-      _text = ''; // 초기화
+      _text = ''; // 텍스트 초기화
       notifyListeners();
 
       await _speech.listen(
-        onResult: (SpeechRecognitionResult result) {
-          print("result: $result");
+        onResult: (SpeechRecognitionResult result) async {
           _text = result.recognizedWords;
-          print("Recognized: $_text");
           notifyListeners();
+
+          // 번역 요청
+          await translateText(_text);
         },
-        localeId: localeId, // 선택된 언어의 localeId로 설정
+        localeId: localeId,
       );
     }
   }
 
   // 음성 인식 중지
-  void stopListening() async {
+  Future<void> stopListening() async {
     if (_isListening) {
       await _speech.stop();
       _isListening = false;
