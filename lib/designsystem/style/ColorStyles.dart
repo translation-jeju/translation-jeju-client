505b1048ea2c07be1f8ff84aed94837436096517
gitsuhyun <suhyun10201@naver.com>
2024-10-31 17:47:10 +0900

    design: ColorStyles.dart

--- lib/designsystem/style/ColorStyles.dart
+++ lib/designsystem/style/ColorStyles.dart
@@ -1,6 +1,14 @@
 import 'package:flutter/material.dart';
 
 class AppColors {
-  // static const Color v1 = Color(0xFFEAE9FE);
-
+  static const Color mainColor = Color(0xFF006557);
+  static const Color subColor = Color(0xFFE6F6F4);
+  static const Color line1 = Color(0xFFE5E5E5);
+  static const Color line2 = Color(0xFFCCCCCC);
+  static const Color line3 = Color(0xFFC3C3C3);
+  static const Color lightText = Color(0xFFE6E6E6);
+  static const Color subText = Color(0xFFBFBFBF);
+  static const Color darkText = Color(0xFF595959);
+  static const Color darkText2 = Color(0xFF737373);
+  static const Color background = Color(0xFFF5F5F5);
 }
