11a75446b668b241bd2cbf704ce114b0c67ced72
glone0506 <glone0506@gmail.com>
2024-10-31 16:19:22 +0900

    feat:언어선택 기능 추가

--- lib/presentation/selectlanguage_view.dart
+++ lib/presentation/selectlanguage_view.dart
@@ -1,28 +1,119 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:translation/riverpod/language_provider.dart';
+import 'package:flutter_gen/gen_l10n/app_localizations.dart';
 
-class SelectLanguageView extends StatelessWidget {
+class SelectLanguageView extends ConsumerWidget {
   const SelectLanguageView({Key? key}) : super(key: key);
 
   @override
-  Widget build(BuildContext context) {
+  Widget build(BuildContext context, WidgetRef ref) {
+    final selectedLanguage = ref.watch(languageProvider);
+
     return Scaffold(
       appBar: AppBar(
-        backgroundColor: Theme.of(context).colorScheme.primary,
+        title: Text(AppLocalizations.of(context)?.hello ?? 'Hello'),
+        backgroundColor: Colors.teal[800],
       ),
       body: Center(
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
-            const Text(
-              'Welcome to Translation Jeju!',
-              style: TextStyle(fontSize: 24),
+            const Padding(
+              padding: EdgeInsets.all(8.0),
+              child: Text(
+                'Please choose your language.',
+                style: TextStyle(
+                  fontSize: 24,
+                  color: Colors.teal,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
             ),
-            const SizedBox(height: 20),
-            ElevatedButton(
-              onPressed: () {},
-              child: const Text('Translate'),
+            LanguageButton(
+              language: Language.korean,
+              text: 'Korean',
+              icon: Icons.park,
+              isSelected: selectedLanguage == Language.korean,
+              onSelect: () =>
+                  ref.read(languageProvider.notifier).state = Language.korean,
+              color: Colors.teal[300]!,
+            ),
+            LanguageButton(
+              language: Language.english,
+              text: 'English',
+              icon: Icons.waves,
+              isSelected: selectedLanguage == Language.english,
+              onSelect: () =>
+                  ref.read(languageProvider.notifier).state = Language.english,
+              color: Colors.blue[300]!,
+            ),
+            LanguageButton(
+              language: Language.nepali,
+              text: 'Nepali',
+              icon: Icons.landscape,
+              isSelected: selectedLanguage == Language.nepali,
+              onSelect: () =>
+                  ref.read(languageProvider.notifier).state = Language.nepali,
+              color: Colors.orange[300]!,
+            ),
+            LanguageButton(
+              language: Language.indonesian,
+              text: 'Indonesian',
+              icon: Icons.sunny,
+              isSelected: selectedLanguage == Language.indonesian,
+              onSelect: () => ref.read(languageProvider.notifier).state =
+                  Language.indonesian,
+              color: Colors.yellow[300]!,
             ),
           ],
+        ),
+      ),
+    );
+  }
+}
+
+class LanguageButton extends StatelessWidget {
+  final Language language;
+  final String text;
+  final IconData icon;
+  final bool isSelected;
+  final VoidCallback onSelect;
+  final Color color;
+
+  const LanguageButton({
+    Key? key,
+    required this.language,
+    required this.text,
+    required this.icon,
+    required this.isSelected,
+    required this.onSelect,
+    required this.color,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.symmetric(vertical: 8.0, horizontal: 16.0),
+      child: ElevatedButton.icon(
+        style: ElevatedButton.styleFrom(
+          backgroundColor: isSelected ? color : Colors.grey[200],
+          foregroundColor: isSelected ? Colors.white : Colors.black87,
+          minimumSize: const Size.fromHeight(50),
+          shape: RoundedRectangleBorder(
+            borderRadius: BorderRadius.circular(15), // 둥근 모서리
+          ),
+          shadowColor: Colors.teal.withOpacity(0.5), // 제주스러운 그림자
+          elevation: isSelected ? 10 : 2, // 선택된 버튼의 그림자를 강조
+        ),
+        onPressed: onSelect,
+        icon: Icon(icon, color: isSelected ? Colors.white : Colors.teal[800]),
+        label: Text(
+          text,
+          style: const TextStyle(
+            fontSize: 18,
+            fontWeight: FontWeight.bold,
+          ),
         ),
       ),
     );
--- macos/Flutter/GeneratedPluginRegistrant.swift
+++ macos/Flutter/GeneratedPluginRegistrant.swift
@@ -5,11 +5,13 @@
 import FlutterMacOS
 import Foundation
 
+import flutter_localization
 import flutter_tts
 import shared_preferences_foundation
 import speech_to_text
 
 func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
+  FlutterLocalizationPlugin.register(with: registry.registrar(forPlugin: "FlutterLocalizationPlugin"))
   FlutterTtsPlugin.register(with: registry.registrar(forPlugin: "FlutterTtsPlugin"))
   SharedPreferencesPlugin.register(with: registry.registrar(forPlugin: "SharedPreferencesPlugin"))
   SpeechToTextPlugin.register(with: registry.registrar(forPlugin: "SpeechToTextPlugin"))
