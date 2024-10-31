a36cc0e880d87fa28380d176bf653a375c9bed15
glone0506 <glone0506@gmail.com>
2024-10-31 13:28:36 +0900

    selectLanguage 페이지 초안

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:translation/presentation/selectlanguage_view.dart';
+import 'package:flutter_localizations/flutter_localizations.dart';
+import 'package:intl/intl.dart';
 
 void main() async {
   //await dotenv.load(fileName: 'assets/config/.env');
@@ -14,6 +16,19 @@ class TranslationApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      localizationsDelegates: const [
+        // 다언어 설정
+        GlobalMaterialLocalizations.delegate,
+        GlobalWidgetsLocalizations.delegate,
+        GlobalCupertinoLocalizations.delegate,
+      ],
+      supportedLocales: const [
+        // 다언어 설정
+        Locale('ko', ''), //한국어
+        Locale('en', ''), //영어
+        Locale('ne', ''), //네팔어
+        Locale('id', ''), //인도네시아어
+      ],
       debugShowCheckedModeBanner: false,
       title: '무사허우꽈',
       theme: ThemeData(
