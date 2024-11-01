5aef12aef9a5e292fec113e23d4cebd6223d2454
gitsuhyun <suhyun10201@naver.com>
2024-11-01 09:59:30 +0900

    feat: list word

--- lib/presentation/ListPage.dart
+++ lib/presentation/ListPage.dart
@@ -64,6 +64,286 @@ class _ListPageState extends ConsumerState<ListPage> {
                     backgroundColor: AppColors.subColor,
                     collapsedBackgroundColor: AppColors.subColor,
                     title: Text(
+                      AppLocalizations.of(context)?.daily_word ?? '실생활 단어',
+                      style: FontStyles.smallBody,
+                    ),
+                    children: [
+                      CustomListTile(
+                        appWord:
+                        AppLocalizations.of(context)?.why ?? 'Welcome',
+                        jejuWord: '무사',
+                        sound: () => _speakText('무사'),
+                        bookmark: () async {
+                          await postBookmark(
+                              AppLocalizations.of(context)!.why.toString(),
+                              '무사', ref);
+                          showToast('저장되었습니다');
+                        },
+                      ),
+                      CustomListTile(
+                        appWord: AppLocalizations.of(context)?.many ??
+                            'Great job',
+                        jejuWord: '하영',
+                        sound: () => _speakText('하영'),
+                        bookmark: () async {
+                          await postBookmark(
+                              AppLocalizations.of(context)!.many.toString(),
+                              '하영', ref);
+                          showToast('저장되었습니다');
+                        },
+                      ),
+                      CustomListTile(
+                        appWord: AppLocalizations.of(context)?.a_little ??
+                            'Enjoy your meal',
+                        jejuWord: '호꼼',
+                        sound: () => _speakText('호꼼'),
+                        bookmark: () async {
+                          await postBookmark(
+                              AppLocalizations.of(context)!.a_little.toString(),
+                              '호꼼', ref);
+                          showToast('저장되었습니다');
+                        },
+                      ),
+                      CustomListTile(
+                        appWord: AppLocalizations.of(context)?.dont_know ??
+                            'Where are you from?',
+                        jejuWord: '알라',
+                        sound: () => _speakText('알라'),
+                        bookmark: () async {
+                          await postBookmark(
+                              AppLocalizations.of(context)!.dont_know.toString(),
+                              '알라', ref);
+                          showToast('저장되었습니다');
+                        },
+                      ),
+                      CustomListTile(
+                        appWord:
+                        AppLocalizations.of(context)?.puppy ?? 'Goodbye',
+                        jejuWord: '강생이',
+                        sound: () => _speakText('강생이'),
+                        bookmark: () async {
+                          await postBookmark(
+                              AppLocalizations.of(context)!.puppy.toString(),
+                              '강생이', ref);
+                          showToast('저장되었습니다');
+                        },
+                      ),
+                      CustomListTile(
+                        appWord:
+                        AppLocalizations.of(context)?.black_stone ?? 'Goodbye',
+                        jejuWord: '거믄둥이',
+                        sound: () => _speakText('거믄둥이'),
+                        bookmark: () async {
+                          await postBookmark(
+                              AppLocalizations.of(context)!.black_stone.toString(),
+                              '거믄둥이', ref);
+                          showToast('저장되었습니다');
+                        },
+                      ),
+                      CustomListTile(
+                        appWord:
+                        AppLocalizations.of(context)?.first ?? 'Goodbye',
+                        jejuWord: '혼저',
+                        sound: () => _speakText('혼저'),
+                        bookmark: () async {
+                          await postBookmark(
+                              AppLocalizations.of(context)!.first.toString(),
+                              '혼저', ref);
+                          showToast('저장되었습니다');
+                        },
+                      ),
+                    ],
+                  ),
+                  ExpansionTile(
+                    backgroundColor: AppColors.subColor,
+                    collapsedBackgroundColor: AppColors.subColor,
+                    title: Text(
+                      AppLocalizations.of(context)?.agriculture ?? '농업',
+                      style: FontStyles.smallBody,
+                    ),
+                    children: [
+                      CustomListTile(
+                        appWord:
+                        AppLocalizations.of(context)?.walking_around ?? 'Welcome',
+                        jejuWord: '도르멍',
+                        sound: () => _speakText('도르멍'),
+                        bookmark: () async {
+                          await postBookmark(
+                              AppLocalizations.of(context)!.walking_around.toString(),
+                              '도르멍', ref);
+                          showToast('저장되었습니다');
+                        },
+                      ),
+                      CustomListTile(
+                        appWord: AppLocalizations.of(context)?.potato ??
+                            'Great job',
+                        jejuWord: '감저',
+                        sound: () => _speakText('감저'),
+                        bookmark: () async {
+                          await postBookmark(
+                              AppLocalizations.of(context)!.potato.toString(),
+                              '감저', ref);
+                          showToast('저장되었습니다');
+                        },
+                      ),
+                      CustomListTile(
+                        appWord: AppLocalizations.of(context)?.black_stone ??
+                            'Where are you from?',
+                        jejuWord: '거믄둥이',
+                        sound: () => _speakText('거믄둥이'),
+                        bookmark: () async {
+                          await postBookmark(
+                              AppLocalizations.of(context)!.black_stone.toString(),
+                              '거믄둥이', ref);
+                          showToast('저장되었습니다');
+                        },
+                      ),
+                      CustomListTile(
+                        appWord:
+                        AppLocalizations.of(context)?.small_thing ?? 'Goodbye',
+                        jejuWord: '작은 거이',
+                        sound: () => _speakText('작은 거이'),
+                        bookmark: () async {
+                          await postBookmark(
+                              AppLocalizations.of(context)!.small_thing.toString(),
+                              '작은 거이', ref);
+                          showToast('저장되었습니다');
+                        },
+                      ),
+                    ],
+                  ),
+                  ExpansionTile(
+                    backgroundColor: AppColors.subColor,
+                    collapsedBackgroundColor: AppColors.subColor,
+                    title: Text(
+                      AppLocalizations.of(context)?.fishing ?? '어업',
+                      style: FontStyles.smallBody,
+                    ),
+                    children: [
+                      CustomListTile(
+                        appWord:
+                        AppLocalizations.of(context)?.please_come_in ?? 'Welcome',
+                        jejuWord: '혼저 옵서예',
+                        sound: () => _speakText('혼저 옵서예'),
+                        bookmark: () async {
+                          await postBookmark(
+                              AppLocalizations.of(context)!.please_come_in.toString(),
+                              '혼저 옵서예', ref);
+                          showToast('저장되었습니다');
+                        },
+                      ),
+                      CustomListTile(
+                        appWord: AppLocalizations.of(context)?.fish ??
+                            'Great job',
+                        jejuWord: '어시',
+                        sound: () => _speakText('어시'),
+                        bookmark: () async {
+                          await postBookmark(
+                              AppLocalizations.of(context)!.fish.toString(),
+                              '어시', ref);
+                          showToast('저장되었습니다');
+                        },
+                      ),
+                      CustomListTile(
+                        appWord: AppLocalizations.of(context)?.urchin_gathering ??
+                            'Enjoy your meal',
+                        jejuWord: '성게치',
+                        sound: () => _speakText('성게치'),
+                        bookmark: () async {
+                          await postBookmark(
+                              AppLocalizations.of(context)!.urchin_gathering.toString(),
+                              '성게치', ref);
+                          showToast('저장되었습니다');
+                        },
+                      ),
+                      CustomListTile(
+                        appWord: AppLocalizations.of(context)?.goodbye ??
+                            'Where are you from?',
+                        jejuWord: '게로마씸',
+                        sound: () => _speakText('게로마씸'),
+                        bookmark: () async {
+                          await postBookmark(
+                              AppLocalizations.of(context)!.goodbye.toString(),
+                              '게로마씸', ref);
+                          showToast('저장되었습니다');
+                        },
+                      ),
+                      CustomListTile(
+                        appWord:
+                        AppLocalizations.of(context)?.enjoy_meal ?? 'Goodbye',
+                        jejuWord: '호꼼 시쿠다마씸',
+                        sound: () => _speakText('호꼼 시쿠다마씸'),
+                        bookmark: () async {
+                          await postBookmark(
+                              AppLocalizations.of(context)!.enjoy_meal.toString(),
+                              '호꼼 시쿠다마씸', ref);
+                          showToast('저장되었습니다');
+                        },
+                      ),
+                    ],
+                  ),
+                  ExpansionTile(
+                    backgroundColor: AppColors.subColor,
+                    collapsedBackgroundColor: AppColors.subColor,
+                    title: Text(
+                      AppLocalizations.of(context)?.animal_husbandry ?? '축산업',
+                      style: FontStyles.smallBody,
+                    ),
+                    children: [
+                      CustomListTile(
+                        appWord:
+                        AppLocalizations.of(context)?.hurry ?? 'Welcome',
+                        jejuWord: '와랑',
+                        sound: () => _speakText('와랑'),
+                        bookmark: () async {
+                          await postBookmark(
+                              AppLocalizations.of(context)!.hurry.toString(),
+                              '와랑', ref);
+                          showToast('저장되었습니다');
+                        },
+                      ),
+                      CustomListTile(
+                        appWord: AppLocalizations.of(context)?.walking_while_working ??
+                            'Great job',
+                        jejuWord: '도르멍 대게',
+                        sound: () => _speakText('도르멍 대게'),
+                        bookmark: () async {
+                          await postBookmark(
+                              AppLocalizations.of(context)!.walking_while_working.toString(),
+                              '도르멍 대게', ref);
+                          showToast('저장되었습니다');
+                        },
+                      ),
+                      CustomListTile(
+                        appWord: AppLocalizations.of(context)?.cow_head ??
+                            'Enjoy your meal',
+                        jejuWord: '솜에이',
+                        sound: () => _speakText('솜에이'),
+                        bookmark: () async {
+                          await postBookmark(
+                              AppLocalizations.of(context)!.cow_head.toString(),
+                              '솜에이', ref);
+                          showToast('저장되었습니다');
+                        },
+                      ),
+                      CustomListTile(
+                        appWord: AppLocalizations.of(context)?.soy_sauce ??
+                            'Where are you from?',
+                        jejuWord: '강작',
+                        sound: () => _speakText('강작'),
+                        bookmark: () async {
+                          await postBookmark(
+                              AppLocalizations.of(context)!.soy_sauce.toString(),
+                              '강작', ref);
+                          showToast('저장되었습니다');
+                        },
+                      ),
+                    ],
+                  ),
+                  ExpansionTile(
+                    backgroundColor: AppColors.subColor,
+                    collapsedBackgroundColor: AppColors.subColor,
+                    title: Text(
                       AppLocalizations.of(context)?.greetings ?? '인사말',
                       style: FontStyles.smallBody,
                     ),
